4853399478af2e2bb4e1b57cbc27436bc3e4688f
DESKTOP-RFRODHF\jung <doizz@naver.com>
2021-07-17 20:57:09 +0900

    모의고사 알고리즘 풀이 리뷰

--- src/programmers/programmers_11.java
+++ src/programmers/programmers_11.java
@@ -11,20 +11,20 @@ public class programmers_11 {
 		int[] person2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
 		int[] person3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
 		int[] arr = new int[3];
-
+		//정답과 일치하는 수포자의 답이 있는지 체크한다.
 		for (int i = 0; i < answers.length; i++) {
 			if (person1[i % person1.length] == answers[i])
-				System.out.println(person1[i % person1.length]);
 				arr[0]++;
 			if (person2[i % person2.length] == answers[i])
 				arr[1]++;
 			if (person3[i % person3.length] == answers[i])
 				arr[2]++;
 		}
-
+		//Math.max함수를 이용하여 많이 많은 수포차를 구한다.
 		int maxValue = Math.max(Math.max(arr[0], arr[1]), arr[2]);
 		ArrayList<Integer> list = new ArrayList<>();
 
+		//많이 많은 숫자를 리스트에 넣는다.
 		if (maxValue == arr[0])
 			list.add(1);
 		if (maxValue == arr[1])
@@ -33,7 +33,7 @@ public class programmers_11 {
 			list.add(3);
 
 		int[] answer = new int[list.size()];
-
+		//정렬하여 많이 맞은 순으로 정렬 후 리턴한다.
 		for (int i = 0; i < answer.length; i++) {
 			answer[i] = list.get(i);
 		}
