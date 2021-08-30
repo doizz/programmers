480e91ec0ab0fb0d4ada0cf865a68084498ab86e
home-PC\home <doizz@naver.com>
2021-08-30 21:28:56 +0900

    유클리드 호제법으로 인한 풀이 리뷰

--- src/programmers/programmers_49.java
+++ src/programmers/programmers_49.java
@@ -9,13 +9,16 @@ public class programmers_49 {
 	public static void main(String[] args) {
 		int n =5;
 		int m =20;
-		 int[] answer = new int[2];
-		   int big = Math.max(n, m);
-	        int small = Math.min(n, m);
-	        
-	        answer[0] = gcd(big, small);
-	        answer[1] = big*small/answer[0];
-	        System.out.println(Arrays.toString(answer));
+		int[] answer = new int[2];
+		//주어진 두 수에 대하여 큰 값과 작은 값을 변수에 대입한다.
+	    int big = Math.max(n, m);
+        int small = Math.min(n, m);
+        
+        //유클리드 호제법으로 최대공약수와 최대공배수를 구한다.
+        answer[0] = gcd(big, small);
+        answer[1] = big*small/answer[0];
+        //값을 리턴한다.
+        System.out.println(Arrays.toString(answer));
 	}
 
 	
